d31afd517b88fbc59f0ddbb4fc425d7776944493
Nikita Bakaev <ya@nbakaev.ru>
2018-03-09 23:14:42 +0300

    refactor

--- src/main/java/com/nbakaev/yad/gateway/youtube/YoutubeDownloader.java
+++ src/main/java/com/nbakaev/yad/gateway/youtube/YoutubeDownloader.java
@@ -2,9 +2,11 @@ package com.nbakaev.yad.gateway.youtube;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -14,17 +16,32 @@ import java.util.regex.Pattern;
 public class YoutubeDownloader {
 
     private final Pattern progressPattern;
+    private final String outputPath;
+    private static final Logger logger = LoggerFactory.getLogger(YoutubeDownloader.class);
 
-    public YoutubeDownloader() {
-        // TODO: check if youtube-dl in PATH
+    public YoutubeDownloader(@Value("${yad.youtube.path-download}") String outputPath) throws IOException, InterruptedException {
+
+        this.outputPath = outputPath;
+        logger.info("Default output path for Youtube {}", this.outputPath);
+
+        // TODO: if youtube-dl in PATH
+        ArrayList<String> objects = new ArrayList<>();
+        objects.add("youtube-dl");
+        objects.add("--version");
+
+        ProcessBuilder processBuilder = new ProcessBuilder(objects);
+        processBuilder.inheritIO();
+        Process p = processBuilder.start();
+
+        p.waitFor();
+        int i = p.exitValue();
+        if (i != 0 ){
+            throw new IOException("youtube-dl is not properly installed");
+        }
         // tested on youtube-dl  >youtube-dl --version //2018.03.03
         progressPattern = Pattern.compile("\\[download\\]\\s+(?<percentage>\\d+(?:\\.\\d+)?%)\\s+of\\s+(?<size>\\d+(?:\\.\\d+)?(?:K|M|G)iB)(?:\\s+at\\s+(?<speed>\\d+(?:\\.\\d+)?(?:K|M|G)iB\\/s))?(?:\\s+ETA\\s+(?<eta>[\\d]{2}:[\\d]{2}))?");
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(YoutubeDownloader.class);
-
-    // TODO: from config
-    private String outputPath = "F:\\test\\%(title)s-%(id)s.%(ext)s";
 
     public Flux<YoutubeUploadStatus> downloadVideo(String id) {
         return Flux.create(sink -> {
@@ -37,7 +54,6 @@ public class YoutubeDownloader {
 
                 ProcessBuilder processBuilder = new ProcessBuilder(objects);
                 Process p = processBuilder.start();
-//            System.out.println(p.getPid());
                 InputStream inputStream = p.getInputStream();
 
                 byte[] buffer = new byte[1000];
---
+++ src/main/java/com/nbakaev/yad/gateway/youtube/YoutubePropertyConfiguration.java
@@ -0,0 +1,14 @@
+package com.nbakaev.yad.gateway.youtube;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "yad.youtube")
+public class YoutubePropertyConfiguration {
+
+    private String pathDownload;
+
+}
