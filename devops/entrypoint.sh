a434d9a8da83c0011773ea3e41230b3735dbe798
Nikita Bakaev <ya@nbakaev.ru>
2019-03-30 23:14:57 +0300

    add cifs-utils #2

---
+++ devops/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -e "$CIFS_PARAMS" ]; then
+    mount -t cifs -o $CIFS_PARAMS
+else
+    echo "No cifs params"
+fi
+
+java -Djava.security.egd=file:/dev/./urandom -jar app.jar
